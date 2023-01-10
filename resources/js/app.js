240ca5ad59844703a3034b7b5b117b5ab278a1e6
CesarF97 <cesarfebres@valdusoft.com>
2023-01-10 14:37:10 -0400

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,7 @@
+import "./bootstrap";
+import { createApp } from "vue";
+import App from "../src/App.vue";
+
+import.meta.glob(["../images/**", "../fonts/**"]);
+
+createApp(App).mount("#app");
---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/css/style-salvia.css',
+                'resources/css/vendor.bundle.css',
+                'resources/js/app.js',
+                'resources/js/jquery.bundle.js',
+                'resources/js/scripts.js',
+                'resources/js/charts.js',
+            ],
+            refresh: true,
+        }),
+        // vue()
+    ],
+});
